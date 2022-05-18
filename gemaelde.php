90de0edc546559a100f6017390a8cae76ba31848
janpo <jan.niklas.pollak@uni-oldenburg.de>
2022-05-18 12:33:21 +0200

    Kommentare schöner

--- gemaelde.php
+++ gemaelde.php
@@ -6,6 +6,7 @@
     <meta name="description" content="Virtuelle Online-Ausstellung von Gemälden">
     <meta name="author" content="Jonas Brüggemann, Ayko Schwedler, Jan Niklas Pollak">
     <link rel="stylesheet" href="css/main.css"/>
+    <link rel="stylesheet" href="css/gemaelde.css"/>
     <title>Online-Ausstellung - Gemälde</title>
 </head>
 
@@ -16,36 +17,77 @@
 
 <main>
 
-    <h1>Titel</h1>
+<h1>Beispiel Gemälde</h1>
 
-    <section>
-        <div>
-            <img alt="Blume" src="images/stockblume_2.jpg" width="150">
-        </div>
-        <div>Beschreibung</div>
-        <h2> Kommentarbereich </h2>
-        <form>
-            <div>
-                <label for="kommentar">Kommentar:</label>
-                <textarea id="kommentar" name="kommentar" cols="40" rows="5" maxlength="1000" wrap="soft"
-                          placeholder="Neuen Kommentar schreiben..." required></textarea>
-                <input type="submit" value="Kommentieren">
+<section>
+    <div>
+        <img alt="Blume" src="images/start.jpg" width="400">
+    </div>
+
+    <div class ="description">
+        <h2>
+            Über das Gemälde
+        </h2>
+
+        <p>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+        </p>
+    </div>
+
+</section>
+<section id = "comment_section">
+    
+    <div class ="align_container">
+        <div class="container">
+            <div>    
+                <h2>Einen Kommentar verfassen</h2>
             </div>
-        </form>
-        <div>
-            <p>Kommentar</p>
+            <form>
+                <textarea id="kommentar" name="kommentar" placeholder="Neuen Kommentar schreiben..." required></textarea>
+                <input type="submit" value="Kommentieren">
+        
+            </form>
         </div>
-        <div>
-            <p>
-                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
-                labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et
-                ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum
-                dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore
-                magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-                clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-            </p>
-        </div>
-    </section>
+    </div>
+    
+
+<ul class="comment-section">
+
+<li class="comment">
+    <div class="info">
+        <a href="#">Tim Wiese</a>
+        <span>4 hours ago</span>
+    </div>
+    <a class="avatar" href="#">
+        <img src="images/start.jpg" width="35" alt="Profile Avatar" />
+    </a>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    </p>   
+
+    <a class= "likes">   
+         <img alt="thumbsUp" src="images/thumbsUp.png" width ="20">
+         12
+    </a>
+</li>
+
+<li class="comment">
+    <div class="info">
+        <a href="#">Ole Eule</a>
+        <span>4 hours ago</span>
+    </div>
+    <a class="avatar" href="#">
+        <img src="images/start.jpg" width="35" alt="Profile Avatar" />
+    </a>
+    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
+    <a class= "likes">   
+         <img alt="thumbsUp" src="images/thumbsUp.png" width ="20">
+         4
+    </a>
+</li>
+</ul>
+
+</section>
 
 </main>
 
