e3160574f612fee1421d6f9c2c48e01e7c7bbed6
Ayko Schwedler <ayko.schwedler@uni-oldenburg.de>
2022-05-29 20:41:57 +0200

    heide yalla

--- php/head.php
+++ php/head.php
@@ -5,14 +5,14 @@
     <meta name="description" content="Virtuelle Online-Ausstellung von Gemälden">
     <meta name="author" content="Jonas Brüggemann, Ayko Schwedler, Jan Niklas Pollak">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/main.css"/>
+    <link rel="stylesheet" href="css/main.css"/>
     <?php
     if (!empty($name)) {
         if ($name == 'Ausstellung' or $name == 'Sammlungen') {
-            echo '<link rel="stylesheet" href="../css/ausstellung.css"/>';
+            echo '<link rel="stylesheet" href="css/ausstellung.css"/>';
         }
         if ($name == 'Gemälde') {
-            echo '<link rel="stylesheet" href="../css/gemaelde.css"/>';
+            echo '<link rel="stylesheet" href="css/gemaelde.css"/>';
         }
         echo '<title>Online-Ausstellung - ' . $name . '</title>';
     } else {
--- php/header.php
+++ php/header.php
@@ -2,17 +2,19 @@
 
 <header>
     <nav>
+        <div class="header-left">
+            <a href="index.php" class="textlogo">Art Place</a>
+        </div>
 
-        <a href="index.php" class="textlogo">Art Place</a>
 
-
-        <a href="ausstellung.php" class="navitem">Ausstellung</a>
-        <a href="sammlungen.php" class="navitem">Sammlungen</a>
-        <a href="registrierung.php" class="navitem">Registrierung</a>
-        <a href="anmeldung.php" class="navitem">Anmeldung</a>
-        <a href="index.php" class="navitem">Abmeldung</a>
-        <a href="profil.php" class="navitem">Mein Profil</a>
-        <a href="neuereintrag.php" class="navitem">Neuer Eintrag</a>
-
+        <div class="header-center">
+            <a href="ausstellung.php" class="navitem">Ausstellung</a>
+            <a href="sammlungen.php" class="navitem">Sammlungen</a>
+            <a href="registrierung.php" class="navitem">Registrierung</a>
+            <a href="anmeldung.php" class="navitem">Anmeldung</a>
+            <a href="index.php" class="navitem">Abmeldung</a>
+            <a href="profil.php" class="navitem">Mein Profil</a>
+            <a href="neuereintrag.php" class="navitem">Neuer Eintrag</a>
+        </div>
     </nav>
 </header>
